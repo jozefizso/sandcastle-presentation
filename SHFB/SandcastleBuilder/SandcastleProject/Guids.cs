e8d1cee5133316def643f0341c44769bd67a69cf
jozef.izso <jozef.izso@localhost>
2010-07-03 14:36:07 +0200

    Created VSPackage project for "Sandcastle Project" extension.

---
+++ SHFB/SandcastleBuilder/SandcastleProject/Guids.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Izsaknet.Sandcastle.VisualStudio
+{
+    static class GuidList
+    {
+        public const string SandcastleProjectPkgString = "59cf444c-3b9d-44f3-abf6-a412beed0684";
+        public const string SandcastleProjectCmdSetString = "0cbb1931-5e92-4ccd-b401-bd15b9b3569f";
+        public const string SandcastleProjectFactoryString = "DBF5A742-155F-495C-A065-F521896F407B";
+
+        public static readonly Guid SandcastleProjectCmdSetGuid = new Guid(SandcastleProjectCmdSetString);
+        public static readonly Guid SandcastleProjectFactoryGuid = new Guid(SandcastleProjectFactoryString);
+    };
+}
---
+++ SHFB/SandcastleBuilder/SandcastleProject/SandcastleProjectFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Izsaknet.Sandcastle.VisualStudio
+{
+    [Guid(GuidList.SandcastleProjectFactoryString)]
+    public class SandcastleProjectFactory
+    {
+    }
+}
