8d1a692f418b0a0248675f92d191979869bbf1aa
jozef.izso <jozef.izso@localhost>
2010-07-22 22:19:49 +0200

    Added RemoveNamespaceDeclarations component that will remove all xmlns declarations in the XHTML documents.

---
+++ SHFB/SandcastleBuilder/SandcastleComponents/RemoveNamespaceDeclarations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+
+using Microsoft.Ddue.Tools;
+
+namespace SandcastleBuilder.Components
+{
+    public class RemoveNamespaceDeclarations : BuildComponent
+    {
+        public RemoveNamespaceDeclarations(BuildAssembler assembler, XPathNavigator configuration)
+            : base(assembler, configuration)
+        {
+        }
+
+        public override void Apply(XmlDocument document, string key)
+        {
+            var attrsToRemove = new List<XmlAttribute>();
+
+            foreach (XmlAttribute attribute in document.DocumentElement.Attributes)
+            {
+                if (attribute.Name.StartsWith("xmlns:"))
+                    attrsToRemove.Add(attribute);
+            }
+
+            attrsToRemove.ForEach(a => document.DocumentElement.Attributes.Remove(a));
+        }
+    }
+}
